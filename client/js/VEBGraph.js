459aea45f8aa987e95091e8d9d1bde0ea79e3912
Firescar96 <nchinda2@gmail.com>
2017-12-02 15:32:44 -0500

    veb tree view

--- client/js/VEBGraph.js
+++ client/js/VEBGraph.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import *as d3 from 'd3';
 import VEBNode from './VEBNode';
+import TreeView from './TreeView';
 const ELEMENT_WIDTH = 30;
 const ELEMENT_HEIGHT = 20;
 require('../sass/veb.scss');
@@ -11,9 +12,8 @@ class vEBGraph extends React.Component {
     this.state = {
       simulation: d3.forceSimulation(),
       newElement: '',
-      root: new VEBNode(8),
+      root: new VEBNode(4),
     };
-    console.log('root', this.state.root);
     this.insertElement = this.insertElement.bind(this);
     this.changeElement = this.changeElement.bind(this);
   }
@@ -51,28 +51,28 @@ class vEBGraph extends React.Component {
     let heightMargin = height / 5;
     let width = veb.node().getBoundingClientRect().width;
 
-    let keys = this.state.root.traversal();
-
-    let clusters = this.state.root.clusterTraversal();
-    console.log(clusters);
-    let treeHeight = Math.log(this.state.root.bits, 2) - 1;
-    clusters.forEach(c => {
-      if(!c.parent) {
-        c.x = width / 2;
-        c.y = heightMargin;
+    let bitvector = this.state.root.bitvector();
+    let treeView = new TreeView(bitvector);
+    let bitNodes = treeView.traversal();
+    console.log('bitvector', bitNodes);
+    let treeHeight = Math.log2(bitvector.length) - 1;
+    bitNodes.forEach(node => {
+      if(!node.parent) {
+        node.x = width / 2;
+        node.y = heightMargin;
         return;
       }
-      //return a list these one for each object element
-      let clusterHeight = Math.log2(c.bits) - 1;
-      let depth = (treeHeight - clusterHeight);
+      let nodeHeight = Math.log2(node.bitvector.length) - 1;
+      let depth = (treeHeight - nodeHeight);
       //align to parent  spacing between clusters
-      c.x = c.parent.x + Math.pow(2, clusterHeight + 1) * c.parentIndex * ELEMENT_WIDTH - Math.pow(2, clusterHeight + 1) * (c.bits - 1) * ELEMENT_WIDTH / 2;
-      c.y = c.parent.y + ELEMENT_HEIGHT * 5;
-      console.log('height', clusterHeight);
-      //console.log((Math.pow(2, clusterHeight) * c.parentIndex) * ELEMENT_WIDTH);
+      let delta = ELEMENT_WIDTH * Math.pow(2, nodeHeight + 1);
+      console.log('delta', node.bitvector, delta, node.parent.x);
+      node.x = node == node.parent.left ? node.parent.x - delta : node.parent.x + delta;
+      console.log('xpos', node.x);
+      node.y = node.parent.y + ELEMENT_HEIGHT * 5;
     });
-    console.log('clusters', clusters);
-    let element = veb.select('#nodes').selectAll('rect.element').data(clusters);
+    console.log('bitnodes', bitNodes);
+    let element = veb.select('#nodes').selectAll('rect.element').data(bitNodes);
 
     element.enter()
       .append('rect')
